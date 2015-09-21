712a140bd3bc0731223fa946be5d7c960c065dd9
mathieumast <mathieu.mast@gmail.com>
2015-09-21 14:03:07 +0200

    Update API

--- web.go
+++ web.go
@@ -21,17 +21,19 @@ func main() {
 	mi := goloc.NewMemindexFromFile(*inputFile)
 
 	router := gin.Default()
+
 	router.GET("/locations/:id", func(c *gin.Context) {
 		loc := mi.Get(c.Params.ByName("id"))
 		c.JSON(200, loc)
 	})
+
 	router.GET("/places/:search", func(c *gin.Context) {
-		if list, err := mi.Search(goloc.NewMapParameters(goloc.KeyValue{Key: "search", Value: c.Params.ByName("search")}, goloc.KeyValue{Key: "limit", Value: 5})); err == nil {
+		if list, err := mi.Search(goloc.Parameters{"search": c.Params.ByName("search"), "limit": 5}); err == nil {
 			c.JSON(200, list.ToArray())
 		} else {
 			c.JSON(500, err)
 		}
-
 	})
+
 	router.Run(":3000")
 }
